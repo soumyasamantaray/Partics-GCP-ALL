6d32d3df1ac72956b58234472e75135b353da705
soumyasamantaray <92103711+soumyasamantaray@users.noreply.github.com>
2023-05-28 14:52:48 +0530

    file aded

---
+++ VM.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "webserver" {
+    name = "webserver"
+    description = "this is 1st server"
+    machine_type = "e2-medium"
+    network_interface {
+      network = "default"
+    }
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-11"
+        labels = {
+            my_label = "debian"
+        }
+      }
+    }
+  
+}
